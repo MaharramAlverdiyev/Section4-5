3a5ce92862c0c387e46652d6430b0bd09b95ce7c
MaharramAlverdiyev <maharramalverdiyev890@gmail.com>
2024-10-20 00:43:45 +0400

    Section4-5

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionFour from './assets/Components/SectionFour/SectionFour'
+import SectionFive from './assets/Components/SectionFive/SectionFive'
+
+const App = () => {
+  return (
+    <div>
+      <SectionFour/>
+      <SectionFive/>
+    </div>
+  )
+}
+
+export default App
